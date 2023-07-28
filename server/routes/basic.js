bfe57426f5ef1b43a17f44c2da8b963cbf4b9e2f
Charles Emakpor <revive23.se@gmail.com>
2023-07-28 10:09:58 +0200

    added communication with esp32

--- server/routes/basic.js
+++ server/routes/basic.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const BASIC_VIEW = 'home';
 const BASIC_VIEW_WITH_PARTIALS = 'homeWithPartials';
 
@@ -17,7 +19,9 @@ export function home(req, res) {
   res.render(BASIC_VIEW, context);
 }
 
-export function homeWithPartials(req, res) {
+export  function homeWithPartials(req, res) {
+
+  const esp32Ip = "192.168.5.4";
   // const context = {
   //   layoutData: {
   //     title: 'My Basic Example',
@@ -32,6 +36,26 @@ export function homeWithPartials(req, res) {
 
   // res.render(BASIC_VIEW_WITH_PARTIALS, context);
 
+  // const a = axios
+  
+    const link =  "http://"+esp32Ip+"/"+req.query.device+"/"+req.query.act;
+    // https://api.neoscan.io/api/main_net/v1/get_all_nodes
+    console.log("the link is ", link);
+
+    try{
+      axios.get(link)
+            .then((data)=> {
+              // res.status(200).send(data)
+              console.log(data);
+              // res.status(200).send(data);
+            })
+            .catch( (err)=>{
+              console.log(err);
+            });
+  }
+  catch(err){
+      console.error("GG", err);
+  }
   res.send("hi");
 }
 
